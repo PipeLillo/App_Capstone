1c74140c55c302936a3e5bd5039845bea6ae1ed7
Patricio Vasquez <patriciovm02@gmail.com>
2025-09-24 18:26:19 -0300

    Google Login, Api Keys, Sql Server (Azure)
    Se agregó el inicio de sesión utilizando Firebase de Google, se añade la sección para colocar las api keys secretas y la integración de la base de datos de Azure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
@@ -11,3 +12,38 @@ export const routes: Routes = [
     pathMatch: 'full',
   },
 ];
+=======
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then(m => m.LoginPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register.page').then(m => m.RegisterPage)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then(m => m.HomePage)
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },  {
+    path: 'calendar',
+    loadComponent: () => import('./pages/calendar/calendar.page').then( m => m.CalendarPage)
+  },
+  {
+    path: 'chatbot',
+    loadComponent: () => import('./pages/chatbot/chatbot.page').then( m => m.ChatbotPage)
+  },
+  {
+    path: 'profile',
+    loadComponent: () => import('./pages/profile/profile.page').then( m => m.ProfilePage)
+  },
+
+];
+>>>>>>> Stashed changes
