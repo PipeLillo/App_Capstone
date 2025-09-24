13d62cee383ab18530db3bfe7e2a195e3ca94bd3
Felipe Andres <fe.lillo@duocuc.cl>
2025-09-23 23:05:56 -0300

    creación de pages principales

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class LoginPage {
+
+  email!: string;
+  password!: string;
+
+  constructor(private router: Router) { }
+
+  async login() {
+    if (!this.email || !this.password) {
+      console.log('Error: Por favor, ingresa tu correo y contraseña.');
+      // Muestra una alerta o un mensaje de error al usuario
+      return;
+    }
+
+    // Lógica temporal que simula un inicio de sesión exitoso
+    console.log('Simulación de inicio de sesión exitoso para el usuario:', this.email);
+    console.log('Redirigiendo a la página de inicio...');
+
+    // Navega a la página de inicio
+    this.router.navigateByUrl('/home', { replaceUrl: true });
+  }
+}
